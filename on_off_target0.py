5aa6924400c250a442cc146c95ea258f460d73ce
benflies <ben.flies.001@student.uni.lu>
2016-05-17 15:37:58 +0200

    -

--- on_off_target0.py
+++ on_off_target0.py
@@ -2,22 +2,24 @@
 # -*- coding: utf-8 -*-
 
 #import modules
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 import pysam
 import re
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import glob
+
 
 #Load files
-REFERENCE = "/media/partition/hg19/ucsc.hg19.fasta"
+REFERENCE = "/media/partition/hg19_broadinstitute/ucsc.hg19.fasta"
 #BAM_FILE = "/media/partition/Haloplex_Test_1_Late_January/Velona/15038519_S3.bam"
-INTERVALS_BED = "/media/partition/Haloplex/genes.bed"
+INTERVALS_BED = "/media/partition/00100-1407755742_Regions.bed"
 #BAM_FILE = "/media/partition/TST15_Test_1_Early_February/Base_Space/BRAF_15018040/Libraries/15018040_S1.bam"
 #INTERVALS_BED = "/media/partition/TST15_Test_1_Early_February/TST_15-A-manifest.bed"
 #BAM_FILE = "/media/partition/15001181_S1.bam"
 #BAM_FILE = "/media/partition/Haloplex/Haloplex_Test_2_Mid_February/Velona/15028422_S6.bam"
-BAM_FILE = "/media/partition/Haloplex/Haloplex_Test_2_Mid_February/Velona/ECD_S9.bam"
+directory = "/media/partition/hpx_csc_velona/"
 #BAM_FILE = "/media/partition/Haloplex_Test_2_Mid_February/Velona/15051669_S1.bam"
 #BAM_FILE = "/media/partition/Haloplex_Test_2_Mid_February/Velona/15020056_S2.bam"
 #BAM_FILE = "/media/partition/Haloplex_Test_2_Mid_February/Velona/15010800_S3.bam"
@@ -49,52 +51,58 @@ if INTERVALS_BED:
 else:
     print("ERROR: Provide an interval list (bed format)")
 
-samfile = pysam.AlignmentFile(BAM_FILE, "rb")
-#total_mapped = samfile.count('chr7', 1, 159138663)
-chr_lengths = samfile.lengths
-#chr_list = samfile.references
-
-chr_list = ['chrM','chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY']
-
-total_mapped_list = []
-on_target_list = []
-off_target_ratios = []
-on_target_ratios = []
+fig,ax1 = plt.subplots()
+plt.hold = True
 
 bam_file_list = [f for f in glob.iglob(directory+"/*.bam")]
 
-boxes = []
+boxes1 = []
+boxes2 = []
 collected = defaultdict(list)
 
 for file in bam_file_list:
 
+    samfile = pysam.AlignmentFile(file, "rb")
+    chr_lengths = samfile.lengths
+
+    chr_list = ['chrM','chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY']
+
+    total_mapped_list = []
+    on_target_list = []
+    off_target_ratios = []
+    on_target_ratios = []
+
+    total_mapped_chr_list = []
+    total_mapped_chr_counter = 0
+
     for i, chr in enumerate(chr_list):
 
-        on_target_counter = 0
-        total_mapped = samfile.count(chr_list[i], 1, chr_lengths[i])
-        total_mapped_list.append(total_mapped)
-        #ratio_on_target = 0
-        #ratio_off_target = 0
+        on_target_chr_counter = 0
+        total_mapped_chr = samfile.count(chr_list[i], 1, chr_lengths[i])
+        total_mapped_chr_counter += total_mapped_chr
+        total_mapped_chr_list.append(total_mapped_chr)
+
         for interval in intervals_list:
             if chr == interval.chr:
-                on_target = samfile.count(interval.chr, interval.start, interval.end)
-                on_target_counter += on_target / total_mapped
-        #if on_target_counter != 0:
-        #    off_target = int(total_mapped) - int(on_target_counter)
-        #    ratio_on_target = float(on_target_counter) / float(total_mapped)
-        #    ratio_off_target = float(off_target) / float(total_mapped)
-        #else:
-        #    ratio_on_target = 0
-        #    ratio_off_target = 1
-        #off_target_ratios.append(ratio_on_target)
-        #on_target_ratios.append(ratio_off_target)
-        #on_target_list.append(on_target_counter)
-        #print('%s : total mapped %s ; mapped in regions %s ; ratio on target: %s ; ratio off target: %s'  %(chr, total_mapped, on_target_counter, ratio_on_target, ratio_off_target))
+                on_target_chr = samfile.count(interval.chr, interval.start, interval.end)
+                on_target_chr_counter += on_target_chr / total_mapped_chr
 
+    boxes1.append(total_mapped_chr_list)
+    boxes2.append(on_target_chr_counter)
+
+plt.boxplot(boxes1,sym='')
+plt.boxplot(boxes2,sym='')
+#xtickNames = plt.setp(ax1, xticklabels = collected.keys())
+#plt.setp(xtickNames, rotation=90, fontsize=7)
+plt.ylabel('Coverage (x)')
+plt.title('Normalized Coverage Distribution per Sample')
+
+plt.show()
+'''
 fig, ax = plt.subplots()
 ind = np.arange(len(on_target_list))
 width = 0.35
-'''
+
 if sys.argv[1] == 'counts':
     total_map = ax.bar(ind, total_mapped_list, color = 'r')
     chr_map = ax.bar(ind + width, on_target_list, color = 'b')
